12e3cfe36e7d4a6d3af067ab3d9639c8f14da4bc
Jeswanth Raghuveer <kanikicharlajeswanthvenkataraghuveer@Jeswanths-MacBook-Air.local>
2026-04-23 21:00:40 +0530

    Merged Leaderboard.sh

---
+++ hub/leaderboard.sh
@@ -0,0 +1,48 @@
+awk '
+BEGIN{
+    FS = ",";
+}
+{
+    wins_player_game[$1 "," $4]++
+    loses_player_game[$2 "," $4]++
+    player[$1,$4]=1
+    player[$2,$4]=1
+}
+END{
+    print "Player, Game, Wins, Losses, Ratio"
+    for ( string in player ){
+        split(string,arr,",")
+
+        game = arr[2]
+        player = arr[1]
+
+        wins = wins_player_game[key] + 0
+        losses = losses_player_game[key] + 0
+
+        if(losses == 0)
+            ratio = "-"
+        else
+            ratio = sprintf("%.2f" , w/l)
+
+        printf "%s,%s,%d,%d,%s\n",
+                player,game,wins,losses,ratio 
+        }
+    
+}
+' history.csv | {
+    read header
+    echo "$header"
+
+    if [ "$sort_by" = "wins" ]; then
+    sort -t',' -k3,3nr
+    elif [ "$sort_by" = "losses" ]; then
+    sort -t',' -k4,4nr
+    elif [ "$sort_by" = "ratio" ]; then
+    sort -t',' -k5,5nr
+    else
+    sort -t',' -k3,3nr
+    fi
+    
+} | column -t -s','
+
+
